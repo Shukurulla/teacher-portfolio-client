0b7c550bc6e7da82f8192a9790607bfec40090e4
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-02-05 14:22:58 +0500

    start

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://teacher-portfolio-server.vercel.app";
 axios.interceptors.request.use((option) => {
   const token = localStorage.getItem("teacher-jwt")
     ? localStorage.getItem("teacher-jwt")
